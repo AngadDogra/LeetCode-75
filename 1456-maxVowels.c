60ea1b71ce84ef43c6039eaf2b2ad835c9ce104e
AngadDogra <angaddogra1204@gmail.com>
2024-07-09 12:48:49 +0530

    1456-maxVowels solved

---
+++ 1456-maxVowels.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isVowel (char c) 
+{
+    return (c == 'a' || c =='e' || c =='i' || c =='o' || c =='u') ;         
+}
+
+int maxVowels(char* s, int k) 
+{
+    // count the vowels in the first substring of length k.
+    int currentVowels = 0 ;
+    int length = strlen(s) ;
+    for (int i = 0 ; i < k ; i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i'|| s[i] == 'o' || s[i] == 'u') {
+            currentVowels++ ;
+        }
+    }
+
+    int maxVowels = currentVowels ;
+    for (int i = k ; i < length ; i++) {
+        if (isVowel(s[i])) {
+            currentVowels++ ;
+        }
+        if (isVowel(s[i-k])) {
+            currentVowels-- ;
+        }
+        if (currentVowels > maxVowels) {
+            maxVowels = currentVowels ;
+        }
+    }
+
+    return maxVowels ;
+}
+
+int main ()
+{
+    char s[] = "abciiidef" ;
+    int result = maxVowels(s, 3) ;
+    printf("The result is %d", result) ; 
+}
